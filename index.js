c2508b4cfbcbf7080e270a02f7e214d50588bf6f
Nicolas Floriano <nicolasfloriano@MacBook-Pro-de-Nicolas.local>
2024-01-24 18:03:57 -0300

    slide added to the form and the function to personalize the name of the list

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+
+$('main header button').click(() => {
+    const checker = $('#formContainer').is(':visible')
+    if (checker) {
+        $('#formContainer').slideUp()
+    } else {
+        $('#formContainer').slideDown()
+    }
+})
+
+$('#pencilEditor').click(() => {
+    let promptValue = prompt('Digite o novo nome da lista. (Max 20 caracteres)')
+    if (promptValue.length > 20) {
+        alert('O nome da lista não pode ter mais de 20 caracteres.')
+    }else if (promptValue.length === 0) {
+        alert('O nome da lista não pode ser vazio.')
+    }else {
+        $('header div h1').text(promptValue)
+    }
+})
